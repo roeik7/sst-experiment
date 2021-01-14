f5314fcec4e9edce8d11c62c0ae8b0e0faf6cdf5
Roei Kalanski <roikln7@gmail.com>
2021-01-14 10:55:27 +0200

    remove playground

--- playground/src/app.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SSTExperiment from './components/SSTExperiment';
-import 'normalize.css/normalize.css'
-import './styles/styles.scss'
-  
-  
-
-  ReactDOM.render(<SSTExperiment />, document.getElementById('app'));
